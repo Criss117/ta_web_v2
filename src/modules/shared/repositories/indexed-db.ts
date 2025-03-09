8ca117244340e99525c5a52a0fee054602e0f8df
Cristian <95005685+Criss117@users.noreply.github.com>
2025-03-09 12:47:04 -0500

    fix: lint errors fixed

--- src/modules/shared/repositories/indexed-db.ts
+++ src/modules/shared/repositories/indexed-db.ts
@@ -1,57 +1,56 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import Dexie, { Table } from "dexie";
 
 const DB_NAME = "ta_web_v2";
 
 interface Model {
-  name: string;
-  schema: any;
-  init: (table: Table<any>) => void;
+	name: string;
+	schema: any;
+	init: (table: Table<any>) => void;
 }
 
 class IndexedDB extends Dexie {
-  private isReady: boolean = false;
+	private isReady: boolean = false;
 
-  constructor() {
-    super(DB_NAME);
-  }
+	constructor() {
+		super(DB_NAME);
+	}
 
-  public initializeDB(models: Model[]) {
-    if (this.isReady) {
-      return;
-    }
+	public initializeDB(models: Model[]) {
+		if (this.isReady) {
+			return;
+		}
 
-    if (this.isOpen()) {
-      this.close();
-    }
+		if (this.isOpen()) {
+			this.close();
+		}
 
-    const modelsToCreate = models
-      .map((model) => ({
-        ...model,
-        schema: this.createSchema(model.schema),
-      }))
-      .reduce(
-        (acc, model) => {
-          acc[model.name] = model.schema;
-          return acc;
-        },
-        {} as Record<string, string>
-      );
+		const modelsToCreate = models
+			.map((model) => ({
+				...model,
+				schema: this.createSchema(model.schema),
+			}))
+			.reduce(
+				(acc, model) => {
+					acc[model.name] = model.schema;
+					return acc;
+				},
+				{} as Record<string, string>,
+			);
 
-    this.version(1).stores(modelsToCreate);
+		this.version(1).stores(modelsToCreate);
 
-    models.forEach((model) => {
-      model.init(this.table(model.name));
-    });
+		models.forEach((model) => {
+			model.init(this.table(model.name));
+		});
 
-    this.isReady = true;
-  }
+		this.isReady = true;
+	}
 
-  public createSchema(schemaObject: Record<string, unknown>) {
-    return Object.keys(schemaObject)
-      .map((key) => (key === "id" ? "id++" : key))
-      .join(", ");
-  }
+	public createSchema(schemaObject: Record<string, unknown>) {
+		return Object.keys(schemaObject)
+			.map((key) => (key === "id" ? "id++" : key))
+			.join(", ");
+	}
 }
 
 export const DB = new IndexedDB();
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,19 +1,19 @@
-import { Toaster } from "@ui/sonner";
 import { createRootRoute, HeadContent, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { Toaster } from "@ui/sonner";
 import { DB } from "@shared/repositories";
 import { Models } from "@shared/config/models.config";
 
 export const Route = createRootRoute({
-  beforeLoad: () => {
-    DB.initializeDB(Models);
-  },
-  component: () => (
-    <>
-      <Outlet />
-      <HeadContent />
-      <TanStackRouterDevtools />
-      <Toaster />
-    </>
-  ),
+	beforeLoad: () => {
+		DB.initializeDB(Models);
+	},
+	component: () => (
+		<>
+			<Outlet />
+			<HeadContent />
+			<TanStackRouterDevtools />
+			<Toaster />
+		</>
+	),
 });
